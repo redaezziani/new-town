5cb474ed5dbc3580cd17288b0f49522807125bc6
redaezziani <2003100500167@ofppt-edu.ma>
2024-04-20 15:35:59 +0000

    Refactor UI styles in products-page and remove unused code

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,18 +1,5 @@
-'use client';
-import { useToast } from "@/components/ui/use-toast";
-
 import ProductsTable from "@/components/user/products/products-table"
-
-
-
 const ProductsPage = async () => {
-  const {toast} = useToast()
-  const handleToast = () => {
-    toast({
-      title: "An error occurred",
-      description: "Something went wrong",
-    });
-  };
   return (
     <main
       className='w-full flex z-10  h-fit min-h-screen overflow-hidden  relative justify-start items-start gap-3 flex-col'
@@ -21,12 +8,7 @@ const ProductsPage = async () => {
       <div className="w-full mt-2  flex-col gap-2 justify-start items-start">
       <ProductsTable />
       </div>
-       <button
-        onClick={handleToast}
-        className="bg-primary text-white px-4 py-2 rounded-md"
-      >
-        Show Toast
-      </button>
+     
     </main>
   )
 }
