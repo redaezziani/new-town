bc63899ce2a7858ca5e88f5114dbaa2195f49c7b
redaezziani <2003100500167@ofppt-edu.ma>
2024-04-23 20:24:53 +0000

    Update GET function in orders route to retrieve products based on order quantity

--- src/app/api/cron/orders/route.ts
+++ src/app/api/cron/orders/route.ts
@@ -5,7 +5,17 @@ import { NextResponse,NextRequest } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse): Promise<void | Response> {
     try {
-        const products = await db.products.findMany();
+        const products = await db.products.findMany({
+            where: {
+                OrderItem: {
+                    every: {
+                        quantity: {
+                            gt: 0
+                        }
+                    }
+                }
+            }
+        }); // this is the query that will get all the products that have been ordered
         return Response.json({ status: 'success', products , message: 'user found' });
     }
      catch (error) {
