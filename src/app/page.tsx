e26f48ac2d962db6492b9ed9fc2abb8ccdb6de0b
redaezziani <2003100500167@ofppt-edu.ma>
2024-04-30 14:29:26 +0000

    Update styling in globals.css, page.tsx, and side-bar.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ const AuroraHero = () => {
 
         </motion.div>
       </AnimatePresence>
-      {/* <BentoDemo /> */}
+      <BentoDemo />
       <div className="w-full mt-20 flex justify-center items-center flex-col gap-2">
         <p
           className=" text-lg max-w-[33rem] text-center text-[#b4bcd0b9]"
